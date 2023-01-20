eabf3e835c12fab5f712fb771dae0c64c184d904
aLoongQwQ <moe@mc-m.net>
2023-01-20 12:49:17 +0800

    awa

--- open.bat
+++ open.bat
@@ -1,9 +1,14 @@
 @echo off
+
 title Vmess++ Connect USBClient ver-Beta1.0
+
 reg query "hklm\software\Python\pythoncore\3.7>2 nul>nul
 if %errorlevel% == 0(goto client) else (echo 请安装python3.7后运行)
+
 :while1
 pause>nul
 goto while1
+
+
 :client
 python init.py
